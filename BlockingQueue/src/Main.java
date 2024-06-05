2e0bd6c20db3058f9a514eee6f32834889c11716
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-05 10:49:52 -0400

    Create 2 threads to test

--- BlockingQueue/src/Main.java
+++ BlockingQueue/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BlockingQueue<Integer> intBQ = new BlockingQueue<>(5);
+        Thread producer = new Thread(() -> {
+            try{
+                for(int i = 0; i < 10; i++){
+                    intBQ.put(i);
+                    System.out.println("Produced: " + i);
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Producer interrupted");
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            try{
+                for(int i = 0; i < 10; i++){
+                    int item = intBQ.take();
+                    System.out.println("Consumed: " + item);
+                    Thread.sleep(150);
+                }
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                System.out.println("Consumer interrupted");
+            }
+        });
+
+        producer.start();
+        consumer.start();
     }
 }
