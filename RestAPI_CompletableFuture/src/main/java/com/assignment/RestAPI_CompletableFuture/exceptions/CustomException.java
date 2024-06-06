edf3527cb00932702e55009c98ea72dc7456d32e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 00:21:26 -0400

    Create CustomException

---
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/exceptions/CustomException.java
@@ -0,0 +1,7 @@
+package com.assignment.RestAPI_CompletableFuture.exceptions;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String msg){
+        super(msg);
+    }
+}
