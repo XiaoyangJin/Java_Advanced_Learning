31ea87bd5a5380e05000637b871c728537ffb494
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-05 23:01:22 -0400

    Configure RestTemplate

---
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.assignment.RestAPI_CompletableFuture.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
