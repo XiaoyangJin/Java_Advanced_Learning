5755a10154e28a995ed0caef01b126a23f7cfeec
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 09:04:25 -0400

    Initiate Rest Controller

---
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/controller/UniversityController.java
@@ -0,0 +1,9 @@
+package com.assignment.RestAPI_CompletableFuture.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/university")
+public class UniversityController {
+}
