e1fbad5ed4385df7830fe0d31c9e81ba9f906d9e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-05 22:29:19 -0400

    Create service logic for 2 api

---
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/service/UniversityService.java
@@ -0,0 +1,15 @@
+package com.assignment.RestAPI_CompletableFuture.service;
+
+import com.assignment.RestAPI_CompletableFuture.entity.University;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public interface UniversityService {
+    //1st api: search by name
+    List<University> getAllUniversities();
+    //2nd api: accept list of countries and send request using multithreading
+    CompletableFuture<List<University>> getAllUniversitiesByCountries(List<String> countries);
+}
