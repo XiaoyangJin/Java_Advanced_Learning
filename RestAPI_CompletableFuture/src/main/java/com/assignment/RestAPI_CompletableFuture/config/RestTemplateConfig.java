8a6570c557c47c5a43e8fdd38e2302aea5cde900
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 00:08:39 -0400

    Change the annotation to qualifier

--- RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/config/RestTemplateConfig.java
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.assignment.RestAPI_CompletableFuture.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @Qualifier("restTemplate")
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/service/impl/UniversityServiceImpl.java
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/service/impl/UniversityServiceImpl.java
@@ -3,6 +3,7 @@ package com.assignment.RestAPI_CompletableFuture.service.impl;
 import com.assignment.RestAPI_CompletableFuture.entity.University;
 import com.assignment.RestAPI_CompletableFuture.service.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,8 +14,7 @@ import java.util.concurrent.CompletableFuture;
 public class UniversityServiceImpl implements UniversityService {
     private final RestTemplate restTemplate;
 
-    @Autowired
-    public UniversityServiceImpl(RestTemplate restTemplate) {
+    public UniversityServiceImpl(@Qualifier("restTemplate") RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 }
