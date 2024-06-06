b956fd572a809723af4e473921ebce90d266e04e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-05 21:48:14 -0400

    Create basic University Entity Structure

---
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/entity/University.java
@@ -0,0 +1,23 @@
+package com.assignment.RestAPI_CompletableFuture.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class University {
+
+    @Id
+    private int university_id;
+
+    private String name;
+    private String domain;
+    private String webPages;
+}
