cf818decc951441c98b88b5c3153ca395e73a31d
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-05 22:49:07 -0400

    Delete Entitry annotation and Id since there is no id field in 3rd party api

--- RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/entity/University.java
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/entity/University.java
@@ -7,16 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class University {
-
-    @Id
-    private int university_id;
-
     private String name;
     private String domain;
     private String web_pages;
