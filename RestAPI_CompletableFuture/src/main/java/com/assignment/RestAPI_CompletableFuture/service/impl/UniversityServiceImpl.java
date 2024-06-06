4a46ff0a0f174e2da0e2ee80349d1ac37fa8eb5d
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 00:05:06 -0400

    Configure RestTemplate in service implementation

---
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/service/impl/UniversityServiceImpl.java
@@ -0,0 +1,20 @@
+package com.assignment.RestAPI_CompletableFuture.service.impl;
+
+import com.assignment.RestAPI_CompletableFuture.entity.University;
+import com.assignment.RestAPI_CompletableFuture.service.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class UniversityServiceImpl implements UniversityService {
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public UniversityServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+}
