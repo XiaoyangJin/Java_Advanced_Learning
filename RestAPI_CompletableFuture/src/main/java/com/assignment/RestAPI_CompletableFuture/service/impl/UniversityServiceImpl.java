2a5fae4b5a9f5ab6d6c7beea6baf531531c2c45c
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 08:18:00 -0400

    Add first business log implementation

--- RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/service/impl/UniversityServiceImpl.java
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/service/impl/UniversityServiceImpl.java
@@ -1,7 +1,9 @@
 package com.assignment.RestAPI_CompletableFuture.service.impl;
 
 import com.assignment.RestAPI_CompletableFuture.entity.University;
+import com.assignment.RestAPI_CompletableFuture.exceptions.CustomException;
 import com.assignment.RestAPI_CompletableFuture.service.UniversityService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,26 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Service
+@Slf4j
 public class UniversityServiceImpl implements UniversityService {
     private final RestTemplate restTemplate;
 
     public UniversityServiceImpl(@Qualifier("restTemplate") RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public List<University> getAllUniversities() {
+        try{
+            String url = "http://universities.hipolabs.com/search";
+            University[] universities = restTemplate.getForObject(url, University[].class);
+            if(universities == null){
+                throw new CustomException("Found Nothing");
+            }
+            return List.of(universities);
+        } catch(Exception e){
+            log.error("Error fetching all universities from first api(getAllUniversities): " + e.getMessage());
+            throw new CustomException("Error fetching all universities from first api(getAllUniversities): " + e.getMessage());
+        }
     }
 }
