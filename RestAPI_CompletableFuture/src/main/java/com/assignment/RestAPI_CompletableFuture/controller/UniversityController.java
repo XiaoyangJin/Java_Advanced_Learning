88ef048f0ce6a1313e46c974b0df29537de51071
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 09:35:23 -0400

    Implement 2 controllers

--- RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/controller/UniversityController.java
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/controller/UniversityController.java
@@ -1,9 +1,31 @@
 package com.assignment.RestAPI_CompletableFuture.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.assignment.RestAPI_CompletableFuture.entity.University;
+import com.assignment.RestAPI_CompletableFuture.service.UniversityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/university")
 public class UniversityController {
+    private final UniversityService universityService;
+
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<University>> getAllUniversities() {
+        List<University> universities = universityService.getAllUniversities();
+        return ResponseEntity.ok(universities);
+    }
+
+    @PostMapping
+    public CompletableFuture<ResponseEntity<List<University>>> getAllUniversitiesByCountries(@RequestBody List<String> countries){
+        return universityService.getAllUniversitiesByCountries(countries)
+                .thenApply(ResponseEntity::ok);
+    }
 }
