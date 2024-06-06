957b294b14cfb35191dcc6768f40cf80ce8883de
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-06 08:13:00 -0400

    Add Repository annotation and comments

--- RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/repository/UniversityRepository.java
+++ RestAPI_CompletableFuture/src/main/java/com/assignment/RestAPI_CompletableFuture/repository/UniversityRepository.java
@@ -2,6 +2,9 @@ package com.assignment.RestAPI_CompletableFuture.repository;
 
 import com.assignment.RestAPI_CompletableFuture.entity.University;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UniversityRepository extends JpaRepository<University, Integer> {
+    // we don't need this if we don't want to store data into database for future use
 }
